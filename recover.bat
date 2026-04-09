a4eeec781922cb75e86811e9ba3dbf9343444818
jangsunwoo <sun475300@gmail.com>
2026-04-09 18:23:40 +0900

    fix: README 구조 복구 + 불필요 파일 20개 제거
    - README: 깨진 Project Structure 섹션 완전 복원 (코드블럭+전체 트리)
    - README: FSM 다이어그램 EMERGENCY 상태 박스 복원
    - 임시/복구/메모 파일 20개 삭제 (.log, .bat, .txt)

--- recover.bat
+++
@@ -1,6 +0,0 @@
-cd /d "C:\Users\sun47\Desktop\swarm-drone-atc"
-echo === REFLOG === > git_recover.log
-git reflog >> git_recover.log 2>&1
-echo === FSCK DANGLING === >> git_recover.log
-git fsck --lost-found --no-reflogs >> git_recover.log 2>&1
-echo === DONE === >> git_recover.log

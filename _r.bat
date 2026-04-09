a3645dab66e4fb0d1f966b6e78db69ff5352a59b
jangsunwoo <sun475300@gmail.com>
2026-04-09 18:42:27 +0900

    12

--- _r.bat
+++
@@ -1,9 +0,0 @@
-cd /d C:\Users\sun47\Desktop\swarm-drone-atc
-git diff --name-status main origin/presentation/portfolio > _filelist.txt 2>&1
-for /f "tokens=1,2" %%a in (_filelist.txt) do (
-    if "%%a"=="A" (
-        git checkout origin/presentation/portfolio -- "%%b" >> restore.log 2>&1
-    )
-)
-echo RESTORE_DONE >> restore.log
-git status --short > status.log 2>&1
